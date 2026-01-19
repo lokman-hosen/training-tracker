c067f797486fa68d2b75d885c18dc5239a43e934
lokman-hosen <lokman@mcc.com.bd>
2026-01-19 14:47:50 +0600

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Super Admin',
+                'phone' => '01700000001',
+                'email' => 'superadmin@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'super-admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Admin',
+                'phone' => '01700000002',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
