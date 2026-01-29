0c255ad66a80121cfe35fb474b15545884d59d43
lokman-hosen <lokman@mcc.com.bd>
2026-01-29 15:37:16 +0600

    Dashboard updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
 //        ]);
         $this->call(UserSeeder::class);
         $this->call(EmployeeSeeder::class);
+        $this->call(TrainingSeeder::class);
     }
 }
